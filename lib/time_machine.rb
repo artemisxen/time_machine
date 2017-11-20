f6ffa3287a761616ec2f4b4117d6eb1a2bb6fc51
artemisxen <artemisxen@gmail.com>
2017-11-20 16:14:13 +0000

    descriptions added to every endpoint

--- lib/time_machine.rb
+++ lib/time_machine.rb
@@ -8,10 +8,12 @@ module TimeMachine
   class API < Grape::API
     format :json
 
+    desc 'reads all the clocks'
     get '/clocks' do
       ClockStore.as_json
     end
 
+    desc 'creates new clock'
     post '/clocks' do
       clock = Clock.new
       ClockStore.add(clock)
@@ -19,13 +21,14 @@ module TimeMachine
       body '{}'
     end
 
+    desc 'reads a clock with a specific id'
     get '/clocks/:id' do
       clock = ClockStore.find(params[:id])
       body clock.as_json
       clock.reduce_counter
     end
 
-    desc 'patch updates the time with a fake time'
+    desc 'updates the time of a specific clock with a fake time'
     params do
       requires :time, regexp: /(?:[1-9]\d{3}-(?:(?:0[1-9]|1[0-2])-(?:0[1-9]|1\d|2[0-8])|(?:0[13-9]|1[0-2])-(?:29|30)|(?:0[13578]|1[02])-31)|(?:[1-9]\d(?:0[48]|[2468][048]|[13579][26])|(?:[2468][048]|[13579][26])00)-02-29)T(?:[01]\d|2[0-3]):[0-5]\d:[0-5]\d(?:Z|[+-][01]\d:[0-5]\d)/
       requires :counter, type: Integer
@@ -35,6 +38,7 @@ module TimeMachine
       body '{}'
     end
 
+    desc 'deletes a clock with a specific id'
     delete '/clocks/:id' do
       ClockStore.delete(params[:id])
       body '{}'
